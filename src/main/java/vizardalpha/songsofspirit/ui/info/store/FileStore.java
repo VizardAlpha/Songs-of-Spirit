bfc094dd967f17c3b098ee11f8e78daed1c3464d
VizardAlpha <43859764+VizardAlpha@users.noreply.github.com>
2024-08-29 22:32:48 +0200

    Fix on master changelogs, thx Argoon

--- src/main/java/vizardalpha/songsofspirit/ui/info/store/FileStore.java
+++ src/main/java/vizardalpha/songsofspirit/ui/info/store/FileStore.java
@@ -1,5 +1,7 @@
 package vizardalpha.songsofspirit.ui.info.store;
 
+import vizardalpha.songsofspirit.log.Logger;
+import vizardalpha.songsofspirit.log.Loggers;
 import vizardalpha.songsofspirit.util.FileUtil;
 import vizardalpha.songsofspirit.game.api.GameModApi;
 
@@ -10,8 +12,16 @@ import java.util.Optional;
 public abstract class FileStore {
     protected final static GameModApi gameModApi = GameModApi.getInstance();
 
+    private final static Logger log = Loggers.getLogger(FileStore.class);
+
     protected static Optional<List<String>> load(String name) {
         Path modPath = gameModApi.getCurrentModPath();
+
+        if (modPath == null) {
+            log.warn("Could not load file %s, because current mod information isn't present.", name);
+            return Optional.empty();
+        }
+
         return FileUtil.load(modPath, name);
     }
 }
